c32c1e213ca06d5591dee80d2bf53851fa6dca07
Stanley0526 <zxc8915138@gmail.com>
2023-08-07 12:33:07 +0800

    RWD修正

---
+++ js/all.js
@@ -0,0 +1,26 @@
+
+
+
+
+time_is_widget.init({Taitung_City_z43f:{}});
+
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+// if (prevScrollpos >= currentScrollPos) {
+//     $(".top").slideDown(200);
+//     // document.querySelector(".top").classList.remove("hidden");
+
+// } else {
+//     // document.querySelector(".top").classList.add("hidden");
+//     $(".top").slideUp(300);
+// }
+// prevScrollpos = currentScrollPos;
+// };
+
+
+
+  
+
+
